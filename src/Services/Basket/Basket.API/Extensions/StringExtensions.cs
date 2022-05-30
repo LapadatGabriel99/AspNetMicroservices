1ffc117a388cd48a963993250cdbf526cede5ded
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-05-31 02:33:23 +0300

    Added repository layer. Implementation for Basket repository

---
+++ src/Services/Basket/Basket.API/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string content)
+        {
+            return string.IsNullOrEmpty(content) || string.IsNullOrEmpty(content);
+        }
+    }
+}
