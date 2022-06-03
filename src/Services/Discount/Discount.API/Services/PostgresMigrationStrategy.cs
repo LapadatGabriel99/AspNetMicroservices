726fc6ffcb132f67ac6268650ec03013e05bdb98
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-06-03 16:52:34 +0300

    Added IDdMigrationStrategy, and PostgresDbMigrationStaregy modules.

---
+++ src/Services/Discount/Discount.API/Services/PostgresMigrationStrategy.cs
@@ -0,0 +1,17 @@
+﻿using Discount.API.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Services
+{
+    internal sealed class PostgresMigrationStrategy : IDbMigrationStrategy
+
+    {
+        public void Migrate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
