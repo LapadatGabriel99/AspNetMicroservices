6f7d705b194b70f3ee71c3b256f71de2a054297a
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-06-04 03:26:17 +0300

    Added discount service functionality.

---
+++ src/Services/Discount/Discount.GRPC/Repositories/DiscountRepository.cs
@@ -0,0 +1,87 @@
+﻿using Dapper;
+using Discount.GRPC.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.GRPC.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly ILogger<DiscountRepository> _logger;
+
+        public DiscountRepository(IConfiguration configuration, ILogger<DiscountRepository> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var result = await connection.ExecuteAsync(
+                "INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)",
+                new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+
+            if (result == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var result = await connection.ExecuteAsync(
+                "DELETE FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
+
+            if (result == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task<Coupon> GetDisount(string productName)
+        {
+            using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>(
+                            "SELECT * FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
+
+            if (coupon == null)
+            {
+                return new Coupon() { ProductName = "No Discount", Amount = 0, Description = "No discount description" };
+            }
+
+            return coupon;
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var result = await connection.ExecuteAsync(
+                "UPDATE Coupon SET ProductName= @ProductName, Description = @Description, Amount = @Amount WHERE Id = @Id",
+                new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount, Id = coupon.Id });
+
+            if (result == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
