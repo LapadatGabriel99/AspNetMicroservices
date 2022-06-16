07834d482b1246dff5892dc719c4f60fbca75545
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-06-17 01:10:21 +0300

    - Added GetOrdersList query and query handler

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersListQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GerOrdersListQuery, List<OrderDto>>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrderDto>> Handle(GerOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderRepository.GetOrderByUserName(request.UserName);
+            var ordersDto = _mapper.Map<List<OrderDto>>(orders);
+
+            return ordersDto;
+        }
+    }
+}
