404ffde2ded03dfcb920a37baa8858f6837dcd55
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-06-17 02:50:27 +0300

    - Added  CheckoutOrderCommand, and CheckoutOrderCommandHandler, and CheckoutOrderCommandValidator.

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Internal;
+using Ordering.Application.Internal.Contracts;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand, int>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        private readonly IMapper _mapper;
+
+        private readonly IEmailService _emailService;
+
+        private readonly ILogger<CheckoutOrderCommandHandler> _logger;
+        private readonly IEmailBuilder _emailBuilder;
+
+        public CheckoutOrderCommandHandler(
+            IOrderRepository orderRepository,
+            IMapper mapper, 
+            IEmailService emailService, 
+            ILogger<CheckoutOrderCommandHandler> logger,
+            IEmailBuilder emailBuilder)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _emailService = emailService;
+            _logger = logger;
+            _emailBuilder = emailBuilder;
+        }
+
+        public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            var result = await AddOrderAsync(request);
+
+            _logger.LogInformation($"Order {result.Id} is successfully created");
+
+            await SendEmail();
+
+            return result.Id;
+        }
+
+        private async Task<Order> AddOrderAsync(CheckoutOrderCommand request)
+        {
+            var orderEntity = _mapper.Map<Order>(request);
+
+            return await _orderRepository.AddAsync(orderEntity);
+        }
+
+        private async Task SendEmail()
+        {
+            await _emailService.SendEmail(
+                _emailBuilder
+                .To("lapadatrobert123@gmail.com")
+                .Subject("Order Api")
+                .Body("Order was created").Create());
+        }
+    }
+}
